ed6eb33b0b445cb6fb4686390024842c290ac666
ZMCodi Mac <zaim.mohsin@gmail.com>
2026-03-09 19:04:02 +0000

    initial commit + knn

---
+++ knn/knn.py
@@ -0,0 +1,204 @@
+from typing import Callable, Literal
+from collections import Counter
+import heapq
+
+import numpy as np
+
+def inner_prod(p: np.ndarray, q: np.ndarray):
+    return np.dot(p, q)
+
+def norm(p: np.ndarray):
+    return np.sqrt(inner_prod(p, p))
+
+def cosine_similarity(p: np.ndarray, q: np.ndarray):
+    return inner_prod(p, q) / (norm(p) * norm(q)) \
+    if norm(p) * norm(q) != 0 else 0
+
+def euclidean_dist(p: np.ndarray, q: np.ndarray):
+    return np.sqrt(np.square(p - q).sum())
+
+def cosine_dist(p: np.ndarray, q: np.ndarray):
+    return 1 - cosine_similarity(p, q)
+
+def RMSE(actual: np.ndarray, pred: np.ndarray):
+    return np.sqrt(np.square(actual - pred).mean())
+
+def MAE(actual: np.ndarray, pred: np.ndarray):
+    return np.abs(actual - pred).mean()
+
+def MAPE(actual: np.ndarray, pred: np.ndarray):
+    return np.abs((actual - pred) / (actual)).mean()
+
+def confusion_matrix(actual: np.ndarray, pred: np.ndarray):
+    actual = actual.flatten()
+    pred = pred.flatten()
+
+    classes = np.unique(actual)
+    class_to_idx = {c: i for i, c in enumerate(classes)}
+
+    mat = np.zeros(shape=(len(classes), len(classes))).astype(int)
+    for i in range(len(actual)):
+        a = actual[i]
+        p = pred[i]
+        mat[class_to_idx[a]][class_to_idx[p]] += 1
+
+    return mat
+
+def analyze_confusion_matrix(mat: np.ndarray):
+    mat_sum = mat.sum()
+
+    precisions = []
+    recalls = []
+    f1s = []
+    accuracies = []
+    specificities = []
+
+    for i in range(len(mat)):
+        tp = mat[i, i]
+        fn = mat[i, :].sum() - tp
+        fp = mat[:, i].sum() - tp
+        tn = mat_sum - tp - fn - fp
+
+        precisions.append(tp / (tp + fp) if tp + fp != 0 else 0)
+        recalls.append(tp / (tp + fn))
+        f1s.append((2 * precisions[i] * recalls[i]) / (precisions[i] + recalls[i])
+                    if precisions[i] + recalls[i] != 0 else 0)
+        accuracies.append((tp + tn) / mat_sum)
+        specificities.append(tn / (tn + fp))
+
+    precisions = np.array(precisions)
+    recalls = np.array(recalls)
+    f1s = np.array(f1s)
+    accuracies = np.array(accuracies)
+    specificities = np.array(specificities)
+
+    return precisions.mean(), recalls.mean(), f1s.mean(), accuracies.mean(), specificities.mean()
+
+class KNN:
+    def __init__(
+        self,
+        task: Literal["R", "C"],
+        data: list[tuple[np.ndarray, np.ndarray]],
+        k: int
+    ):
+        """
+        task: "R" for regression, "C" for classification
+        data: List of (input, output) pairs
+        k: number of neighbors to consider
+        """
+        self.data: list[tuple[np.ndarray, np.ndarray]] = data
+        self.k = k
+        self.task = task
+
+    def test(self, test_data: list[tuple[np.ndarray, np.ndarray]], print_=True):
+        """
+        test_data: list of (input, actual output) pairs to test on the model
+        """
+        ins = [x for x, _ in test_data]
+        actual = np.array([y for _, y in test_data])
+        predictions = np.array(self.predict(ins))
+
+        actual = np.array(actual)
+        prediction = np.array(predictions)
+
+        if self.task == "R":
+            # calculate RMSE, MAE, MAPE
+            rmse = RMSE(actual, prediction)
+            if print_:
+                print(f"RMSE: {rmse:.6f}")
+                print(f"MAE: {MAE(actual, prediction):.6f}")
+                print(f"MAPE: {MAPE(actual, prediction):.6f}")
+            return -rmse
+
+        elif self.task == "C":
+            # calculate accuracy, error
+            conf_mat = confusion_matrix(actual, prediction)
+            prec, recall, f1, acc, spec = analyze_confusion_matrix(conf_mat)
+            if print_:
+                print(f"Confusion matrix:")
+                print(conf_mat)
+                print(f"Accuracy: {acc:.6f}")
+                print(f"Error: {1 - acc:.6f}")
+                print(f"Precision: {prec:.6f}")
+                print(f"Recall: {recall:.6f}")
+                print(f"F1 score: {f1:.6f}")
+                print(f"Specificity: {spec:.6f}")
+            return f1
+
+    def predict(self, x: list[np.ndarray], distance_f: Callable = euclidean_dist) -> list[np.ndarray]:
+        """
+        x: list of input vectors to predict
+        Returns: list of output vectors
+        """
+        output: list[np.ndarray] = []
+
+        for inp in x:
+            # max heap of (distance, index (tiebreaker), output)
+            heap: list[tuple[float, int, np.ndarray]] = []
+
+            for i, (dx, dy) in enumerate(self.data):
+                dist = distance_f(inp, dx)
+
+                # only store the k nearest neighbours
+                if len(heap) < self.k:
+                    heapq.heappush_max(heap, (dist, i, dy))
+                elif dist < heap[0][0]:
+                    heapq.heappop_max(heap)
+                    heapq.heappush_max(heap, (dist, i, dy))
+
+            # now we have the output of k nearest neighbours to x in heap
+            outputs = [y for _, _, y in heap]
+
+            if self.task == "R":
+                # average over all neighbors
+                output.append(np.array(outputs).mean(axis=0))
+
+            elif self.task == "C":
+                # find the most frequent class
+                # y here should be singleton lists
+                classes = np.array(outputs).flatten()
+                mode = Counter(classes).most_common(1)[0][0]
+                output.append(np.array([mode]))
+
+        return output
+
+    def optimize_k(self, k_fold=5, data_set: list[tuple[np.ndarray, np.ndarray]] | None = None):
+        """
+        Optimizes hyperparameter k by maximizing accuracy using k-fold CV
+        k_fold: how many folds to split the data into
+        data_set: optional data set to optimize on. If not provided, uses model's data
+        """
+        initial_data = self.data.copy()
+        initial_k = self.k
+
+        # split data into k_fold chunks
+        full_data = data_set if data_set else self.data
+        chunk_size = len(full_data) // k_fold
+        chunks = [full_data[i:i+chunk_size] for i in range(0, len(full_data), chunk_size)]
+
+        best_k = self.k
+        best_acc = float("-inf")
+
+        # try all odd k
+        for k in range(1, chunk_size * (k_fold - 1), 2):
+            accuracies = []
+            for i, chunk in enumerate(chunks):
+
+                # set i as test and rest as training
+                test_set = chunk
+                self.data = sum(chunks[:i] + chunks[i+1:], [])
+                self.k = k
+
+                accuracies.append(self.test(test_set, print_=False))
+
+            # average accuracies and check if best
+            acc = sum(accuracies) / len(accuracies)
+            print(f"k: {k}, accuracy: {acc}")
+            if acc > best_acc:
+                best_acc = acc
+                best_k = k
+
+        print(f"Best k: {best_k} ({best_acc})")
+        self.data = initial_data
+        self.k = initial_k
+
---
+++ knn/test.py
@@ -0,0 +1,56 @@
+import os
+import csv
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from knn import KNN
+
+DATA_DIR = "data"
+
+# load iris data
+iris_data: list[tuple[np.ndarray, np.ndarray]] = []
+with open(f"{DATA_DIR}/iris/iris.csv", "r") as f:
+    raw_data = list(csv.reader(f))[1:]
+
+    # convert to float
+    for row in raw_data:
+        inp = np.array([float(num) for num in row[:-1]])
+        out = np.array([row[-1]])
+        iris_data.append((inp, out))
+
+# load face data
+face_data: list[tuple[np.ndarray, np.ndarray]] = []
+face_data_dir = DATA_DIR + "/att_faces"
+for folder in os.listdir(face_data_dir):
+    path = face_data_dir + "/" + folder
+    if os.path.isdir(path):
+        for img in os.listdir(path):
+            face = plt.imread(path + "/" + img, "r")
+
+            # split face into half
+            cols = face.shape[-1]
+            first_half = face[:, :cols // 2].flatten()
+            second_half = face[:, cols // 2:].flatten()
+
+            face_data.append((first_half, second_half))
+
+
+np.random.shuffle(iris_data)
+knn = KNN(task="C", data=iris_data[:int(len(iris_data) * .80)], k=3)
+# knn.optimize_k(data_set=iris_data)
+knn.test(test_data=iris_data[int(len(iris_data) * .80):])
+
+inp = iris_data[-1][0]
+predicted = knn.predict([inp])
+print(predicted, iris_data[-1][1])
+
+np.random.shuffle(face_data)
+knn = KNN(task="R", data=face_data[:int(len(face_data) * .80)], k=5)
+# knn.optimize_k(data_set=face_data)
+knn.test(test_data=face_data[int(len(face_data) * .80):])
+
+inp = face_data[-1][0]
+predicted = knn.predict([inp])[0].reshape((112,46))
+plt.imshow(np.concatenate([inp.reshape(112,46), predicted], axis=1), cmap="gray")
+plt.show()
